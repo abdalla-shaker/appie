506a28f28a079fd827571ab5e39728bf89877c22
abdalla-shaker <abdallashaker143@gmail.com>
2024-07-21 12:29:56 +0300

    adding another section and improving the js code

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,34 +1,27 @@
 class Content {
+  svgIcon;
+  h3;
+  p;
+  a;
   color;
-
-  constructor(color) {
+  constructor(svgIcon, h3, p, a, color) {
+    this.svgIcon = svgIcon;
+    this.h3 = h3;
+    this.p = p;
+    this.a = a;
     this.color = color;
   }
 }
 
 const content = {
   content: [
-    new Content("#60A5FA"),
-    new Content("#F87171"),
-    new Content("#1E40AF"),
-    new Content("#F59E0B"),
-    new Content("#3730A3"),
-    new Content("#047857"),
-  ],
-  render() {
-    const rowCards = document.getElementById("row-of-cards-1");
-    for (const cont of this.content) {
-      const card = document.createElement("div");
-      card.className = "card";
-      card.innerHTML = `
-        <div class="logo-container flex-display center" style="background-color: ${cont.color} !important">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="#fff" viewBox="0 0 24 24" stroke-width="1.5" stroke="#fff" class="size-6 message">
-            <path stroke-linecap="round" stroke-linejoin="round" d="m3.75 13.5 10.5-11.25L12 10.5h8.25L9.75 21.75 12 13.5H3.75Z" />
-          </svg>
-        </div>
-        <h3>fast and intuitive</h3>
-        <p>Oxford posh bevvy give us a bell gutted mate spend a penny quaint cockup plastered.</p>
-        <a href="#" class="flex-display align-center">Read More <svg
+    new Content(
+      `<svg xmlns="http://www.w3.org/2000/svg" fill="#fff" viewBox="0 0 24 24" stroke-width="1.5" stroke="#fff" class="size-6 message">
+            <path stroke-linecap="round" stroke-linejoin="round" d="m3.75 13.5 10.5-11.25L12 10.5h8.25L9.75 21.75 12 13.5H3.75Z"></path>
+      </svg>`,
+      "fast and intuitive",
+      "Oxford posh bevvy give us a bell gutted mate spend a penny quaint cockup plastered.",
+      `Read More <svg
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 24 24"
           fill="currentColor"
@@ -38,8 +31,137 @@ const content = {
             fill-rule="evenodd"
             d="M11.03 3.97a.75.75 0 0 1 0 1.06l-6.22 6.22H21a.75.75 0 0 1 0 1.5H4.81l6.22 6.22a.75.75 0 1 1-1.06 1.06l-7.5-7.5a.75.75 0 0 1 0-1.06l7.5-7.5a.75.75 0 0 1 1.06 0Z"
             clip-rule="evenodd"
-          />
+        />
+        </svg>`,
+      "#60A5FA"
+    ),
+    new Content(
+      ` 
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#fff" class="size-6 message">
+          path d="M11.47 3.841a.75.75 0 0 1 1.06 0l8.69 8.69a.75.75 0 1 0 1.06-1.061l-8.689-8.69a2.25 2.25 0 0 0-3.182 0l-8.69 8.69a.75.75 0 1 0 1.061 1.06l8.69-8.689Z" />
+          <path d="m12 5.432 8.159 8.159c.03.03.06.058.091.086v6.198c0 1.035-.84 1.875-1.875 1.875H15a.75.75 0 0 1-.75-.75v-4.5a.75.75 0 0 0-.75-.75h-3a.75.75 0 0 0-.75.75V21a.75.75 0 0 1-.75.75H5.625a1.875 1.875 0 0 1-1.875-1.875v-6.198a2.29 2.29 0 0 0 .091-.086L12 5.432Z" />
         </svg>
+
+      `,
+      "fast and intuitive",
+      "Oxford posh bevvy give us a bell gutted mate spend a penny quaint cockup plastered.",
+      `Read More <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          fill="currentColor"
+          class="size-6 right-arrow"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M11.03 3.97a.75.75 0 0 1 0 1.06l-6.22 6.22H21a.75.75 0 0 1 0 1.5H4.81l6.22 6.22a.75.75 0 1 1-1.06 1.06l-7.5-7.5a.75.75 0 0 1 0-1.06l7.5-7.5a.75.75 0 0 1 1.06 0Z"
+            clip-rule="evenodd"
+        />
+        </svg>`,
+      "#F87171"
+    ),
+    new Content(
+      ` 
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="3" stroke="#fff" class="size-6 message">
+         <path stroke-linecap="round" stroke-linejoin="round" d="M13.19 8.688a4.5 4.5 0 0 1 1.242 7.244l-4.5 4.5a4.5 4.5 0 0 1-6.364-6.364l1.757-1.757m13.35-.622 1.757-1.757a4.5 4.5 0 0 0-6.364-6.364l-4.5 4.5a4.5 4.5 0 0 0 1.242 7.244" />
+        </svg>
+      `,
+      "fast and intuitive",
+      "Oxford posh bevvy give us a bell gutted mate spend a penny quaint cockup plastered.",
+      `Read More <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          fill="currentColor"
+          class="size-6 right-arrow"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M11.03 3.97a.75.75 0 0 1 0 1.06l-6.22 6.22H21a.75.75 0 0 1 0 1.5H4.81l6.22 6.22a.75.75 0 1 1-1.06 1.06l-7.5-7.5a.75.75 0 0 1 0-1.06l7.5-7.5a.75.75 0 0 1 1.06 0Z"
+            clip-rule="evenodd"
+        />
+        </svg>`,
+      "#1E40AF"
+    ),
+    new Content(
+      ` 
+        <svg xmlns="http://www.w3.org/2000/svg" fill="#Fff" viewBox="0 0 24 24" stroke-width="1.5" stroke="#fff" class="size-6 message">
+         <path stroke-linecap="round" stroke-linejoin="round" d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0" />
+        </svg>
+
+      `,
+      "fast and intuitive",
+      "Oxford posh bevvy give us a bell gutted mate spend a penny quaint cockup plastered.",
+      `Read More <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          fill="currentColor"
+          class="size-6 right-arrow"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M11.03 3.97a.75.75 0 0 1 0 1.06l-6.22 6.22H21a.75.75 0 0 1 0 1.5H4.81l6.22 6.22a.75.75 0 1 1-1.06 1.06l-7.5-7.5a.75.75 0 0 1 0-1.06l7.5-7.5a.75.75 0 0 1 1.06 0Z"
+            clip-rule="evenodd"
+        />
+        </svg>`,
+      "#F59E0B"
+    ),
+    new Content(
+      ` 
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#fff" class="size-6 message">
+          <path fill-rule="evenodd" d="M11.078 2.25c-.917 0-1.699.663-1.85 1.567L9.05 4.889c-.02.12-.115.26-.297.348a7.493 7.493 0 0 0-.986.57c-.166.115-.334.126-.45.083L6.3 5.508a1.875 1.875 0 0 0-2.282.819l-.922 1.597a1.875 1.875 0 0 0 .432 2.385l.84.692c.095.078.17.229.154.43a7.598 7.598 0 0 0 0 1.139c.015.2-.059.352-.153.43l-.841.692a1.875 1.875 0 0 0-.432 2.385l.922 1.597a1.875 1.875 0 0 0 2.282.818l1.019-.382c.115-.043.283-.031.45.082.312.214.641.405.985.57.182.088.277.228.297.35l.178 1.071c.151.904.933 1.567 1.85 1.567h1.844c.916 0 1.699-.663 1.85-1.567l.178-1.072c.02-.12.114-.26.297-.349.344-.165.673-.356.985-.57.167-.114.335-.125.45-.082l1.02.382a1.875 1.875 0 0 0 2.28-.819l.923-1.597a1.875 1.875 0 0 0-.432-2.385l-.84-.692c-.095-.078-.17-.229-.154-.43a7.614 7.614 0 0 0 0-1.139c-.016-.2.059-.352.153-.43l.84-.692c.708-.582.891-1.59.433-2.385l-.922-1.597a1.875 1.875 0 0 0-2.282-.818l-1.02.382c-.114.043-.282.031-.449-.083a7.49 7.49 0 0 0-.985-.57c-.183-.087-.277-.227-.297-.348l-.179-1.072a1.875 1.875 0 0 0-1.85-1.567h-1.843ZM12 15.75a3.75 3.75 0 1 0 0-7.5 3.75 3.75 0 0 0 0 7.5Z" clip-rule="evenodd" />
+        </svg>
+      `,
+      "fast and intuitive",
+      "Oxford posh bevvy give us a bell gutted mate spend a penny quaint cockup plastered.",
+      `Read More <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          fill="currentColor"
+          class="size-6 right-arrow"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M11.03 3.97a.75.75 0 0 1 0 1.06l-6.22 6.22H21a.75.75 0 0 1 0 1.5H4.81l6.22 6.22a.75.75 0 1 1-1.06 1.06l-7.5-7.5a.75.75 0 0 1 0-1.06l7.5-7.5a.75.75 0 0 1 1.06 0Z"
+            clip-rule="evenodd"
+        />
+        </svg>`,
+      "#3730A3"
+    ),
+    new Content(
+      ` 
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#fff" class="size-6 message">
+          <path fill-rule="evenodd" d="M12 1.5a5.25 5.25 0 0 0-5.25 5.25v3a3 3 0 0 0-3 3v6.75a3 3 0 0 0 3 3h10.5a3 3 0 0 0 3-3v-6.75a3 3 0 0 0-3-3v-3c0-2.9-2.35-5.25-5.25-5.25Zm3.75 8.25v-3a3.75 3.75 0 1 0-7.5 0v3h7.5Z" clip-rule="evenodd" />
+        </svg>
+      `,
+      "fast and intuitive",
+      "Oxford posh bevvy give us a bell gutted mate spend a penny quaint cockup plastered.",
+      `Read More <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          fill="currentColor"
+          class="size-6 right-arrow"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M11.03 3.97a.75.75 0 0 1 0 1.06l-6.22 6.22H21a.75.75 0 0 1 0 1.5H4.81l6.22 6.22a.75.75 0 1 1-1.06 1.06l-7.5-7.5a.75.75 0 0 1 0-1.06l7.5-7.5a.75.75 0 0 1 1.06 0Z"
+            clip-rule="evenodd"
+        />
+        </svg>`,
+      "#047857"
+    ),
+  ],
+  render() {
+    const rowCards = document.getElementById("row-of-cards-1");
+    for (const cont of this.content) {
+      const card = document.createElement("div");
+      card.className = "card";
+      card.innerHTML = `
+        <div class="logo-container flex-display center" style="background-color: ${cont.color} !important">
+          ${cont.svgIcon}
+        </div>
+        <h3>${cont.h3}</h3>
+        <p>${cont.p}</p>
+        <a href="#" class="flex-display align-center">
+          ${cont.a}
         </a>
       `;
       rowCards.append(card);
